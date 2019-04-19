8adfd8754ecc47577a8c14ee72e0b45856da0be5
Marcelo Ventura <marcelo.ventura@slalom-mv.local>
2019-04-19 14:47:44 -0700

    terraform backend

--- aws.tf
+++ aws.tf
@@ -3,3 +3,11 @@ provider "aws" {
   shared_credentials_file = ""
   profile                 = ""
 }
+
+terraform {
+  backend "s3" {
+    bucket = "gene-repository"
+    key    = "./terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
